e3943357a421ba0f92a3ec3a6c18cf6999d2d486
Ophélie <ophe.iborra@hotmail.fr>
2022-10-28 11:05:35 +0200

    First Version

---
+++ js/fiche_produit.js
@@ -0,0 +1,4 @@
+<script>  <input type="text" value="0" id="nb" />
+    <input type="button" value="+" onclick="document.getElementById('nb').value -= -1;" />
+    <input type="button" value="-" onclick="document.getElementById('nb').value -= 1;" />
+</document.getElementById> </script>
